a08cc6051fe1236efca264d6a6f8818be2d7389b
m1k3y <kenthan@nautic-shift.local>
2017-12-07 01:50:25 -0800

    use clipboard.js

--- copy.js
+++ copy.js
@@ -1,24 +1,8 @@
+var clip = new Clipboard('.btn');
 
-
-
-var copyPhoneBtn = document.querySelector('.js-phonecopybtn');
-copyPhoneBtn.addEventListener('click', function(event) {
-  // Select the email link anchor text
-  var phoneLink = document.querySelector('.js-phonelink');
-  var range = document.createRange();
-  range.selectNode(phoneLink);
-  window.getSelection().addRange(range);
-
-  try {
-    // Now that we've selected the anchor text, execute the copy command
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Copy phone command was ' + msg);
-  } catch(err) {
-    console.log('Oops, unable to copy');
-  }
-
-  // Remove the selections - NOTE: Should use
-  // removeRange(range) when it is supported
-  window.getSelection().removeAllRanges();
+clip.on("success", function() {
+  document.body.insertAdjacentHTML('beforeend', '<div>that worked.</div>');
+});
+clip.on("error", function() {
+  document.body.insertAdjacentHTML('beforeend', '<div>that didn\'t work.</div>');
 });
